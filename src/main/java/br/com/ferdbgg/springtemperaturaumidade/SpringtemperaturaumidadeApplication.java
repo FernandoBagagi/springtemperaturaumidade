f423ac4c215244279411a8926736b8dff384b9ac
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-22 14:56:41 -0300

    cria o projeto

---
+++ src/main/java/br/com/ferdbgg/springtemperaturaumidade/SpringtemperaturaumidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferdbgg.springtemperaturaumidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtemperaturaumidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtemperaturaumidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ferdbgg/springtemperaturaumidade/SpringtemperaturaumidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferdbgg.springtemperaturaumidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtemperaturaumidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
