bbff32fd2350f848878ce9c9222a4db837f530fc
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-26 14:12:57 -0300

    cria LeitorPortaSerialService

---
+++ src/main/java/br/com/ferdbgg/springtemperaturaumidade/LeitorPortaSerialService.java
@@ -0,0 +1,63 @@
+package br.com.ferdbgg.springtemperaturaumidade;
+
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+import br.com.ferdbgg.springtemperaturaumidade.exceptions.LeitorPortaSerialException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LeitorPortaSerialService {
+
+    private final LeituraSensorRepository repository;
+
+    public void iniciarLeituraPortaSerial() {
+        final Thread leitorDadosSerialThread = new Thread(this::lerDadosSerial);
+        leitorDadosSerialThread.setName("Thread-Leitor-Dados-Serial");
+        leitorDadosSerialThread.start();
+    }
+
+    private void lerDadosSerial() {
+
+        try {
+            LeitorPortaSerial.builderLeitor(this::consumerStreamDadosSerial);
+        } catch (LeitorPortaSerialException e) {
+            System.err.println("Erro na comunicação serial: " + e.getMessage());
+        }
+
+    }
+
+    private void consumerStreamDadosSerial(InputStream stream) throws LeitorPortaSerialException {
+
+        try (final var scanner = new Scanner(stream)) {
+
+            while (scanner.hasNextLine()) {
+
+                final var linha = scanner.nextLine();
+                final var dados = linha.split(",");
+
+                if (dados.length != 2) {
+                    System.err.println("Dados inconsistentes");
+                    continue;
+                }
+
+                final var temperatura = new BigDecimal(dados[0].trim());
+                final var umidade = new BigDecimal(dados[1].trim());
+
+                final var leitura = new LeituraSensor(
+                        temperatura,
+                        umidade);
+
+                repository.save(leitura);
+
+            }
+
+        }
+
+    }
+
+}
