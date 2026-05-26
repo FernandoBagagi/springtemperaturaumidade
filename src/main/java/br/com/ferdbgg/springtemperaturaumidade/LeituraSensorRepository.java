4277a24816200c4ae64480c3619c57437c509e52
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-26 14:10:27 -0300

    cria LeituraSensorRepository

---
+++ src/main/java/br/com/ferdbgg/springtemperaturaumidade/LeituraSensorRepository.java
@@ -0,0 +1,8 @@
+package br.com.ferdbgg.springtemperaturaumidade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LeituraSensorRepository extends JpaRepository<LeituraSensor, Long> {
+}
