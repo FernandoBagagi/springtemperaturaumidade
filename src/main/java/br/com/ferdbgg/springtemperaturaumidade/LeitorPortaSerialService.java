c815d4aa9d4882018b348901bb729dcfcb8c7d91
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-26 14:25:18 -0300

    adiciona comportamento de chamar leitor de serial após a inicialização

--- src/main/java/br/com/ferdbgg/springtemperaturaumidade/LeitorPortaSerialService.java
+++ src/main/java/br/com/ferdbgg/springtemperaturaumidade/LeitorPortaSerialService.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import br.com.ferdbgg.springtemperaturaumidade.exceptions.LeitorPortaSerialException;
@@ -15,6 +17,7 @@ public class LeitorPortaSerialService {
 
     private final LeituraSensorRepository repository;
 
+    @EventListener(ApplicationReadyEvent.class)
     public void iniciarLeituraPortaSerial() {
         final Thread leitorDadosSerialThread = new Thread(this::lerDadosSerial);
         leitorDadosSerialThread.setName("Thread-Leitor-Dados-Serial");
