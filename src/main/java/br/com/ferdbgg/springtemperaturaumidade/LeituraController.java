1b8d3e30b33d0236243c46872c239ed958344b77
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-26 14:25:29 -0300

    cria LeituraController

---
+++ src/main/java/br/com/ferdbgg/springtemperaturaumidade/LeituraController.java
@@ -0,0 +1,22 @@
+package br.com.ferdbgg.springtemperaturaumidade;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/leituras")
+@RequiredArgsConstructor
+public class LeituraController {
+
+    private final LeituraSensorRepository repository;
+
+    @GetMapping
+    public List<LeituraSensor> listarHistorico() {
+        return repository.findAll();
+    }
+}
