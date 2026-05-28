e1483564bba5160e2f43a25824a6fbc3d7034941
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-28 12:19:27 -0300

    cria classe entidade Usuario

---
+++ src/main/java/br/com/ferdbgg/springtemperaturaumidade/autenticacao/Usuario.java
@@ -0,0 +1,42 @@
+package br.com.ferdbgg.springtemperaturaumidade.autenticacao;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    
+}
